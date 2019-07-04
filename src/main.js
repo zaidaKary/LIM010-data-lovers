e001412df030d3bc53ff5a3744dcf9fba23dd93b
jorgelisari1 <jorgelis_ari@hotmail.com>
2019-07-04 13:05:43 -0500

    comienzo c

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ const BarraMenu = document.getElementById('barra-menu');
 //Botones
 const BotonIngresar = document.getElementById('BotonIngresar');
 const botonBuscar = document.getElementById('botonBuscar');
+const regresar = document.getElementById('regresar');
 
 //Variable de la Data
 const pokemonNew = nuevaDataPokemones();
@@ -56,23 +57,35 @@ botonBuscar.addEventListener('click', () => {
     unPokemon.classList.remove('hide');
 });
 
+//Funcionalidad del boton regresar 
+    regresar.addEventListener('click', () => {
+    obtenerNombre.value ='';    
+    allPokemones.classList.remove('hide');
+    unPokemon.classList.add('hide');
+
+});
+const MaysPrimera =(string)=>{
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
 //Funcion Buscar Pokemones
 const buscarPorNombre = (allPokemon) => {
     let llamado = '';
 
     for (let i = 0; i < allPokemon.length; i++) {
 
-        if (obtenerNombre.value == allPokemon[i].nombre && i < allPokemon.length) {
-            llamado = `<div>
-             <img src= "${allPokemon[i].imagen}" />
-             <p>Nombre: ${allPokemon[i].nombre}</p>
-             <p>Número: ${allPokemon[i].number}</p>
-             <p>Altura: ${allPokemon[i].altura}</p>
-             <p>Peso: ${allPokemon[i].peso}</p>
-             <p>caramelo: ${allPokemon[i].caramelo}</p>
-             <p>contador de Caramelos: ${allPokemon[i].contadorDeCaramelos}</p>
-             <p>Huevo: ${allPokemon[i].huevo}</p>
-             <p>Tipo: ${allPokemon[i].tipo}</p></div>`;
+        if (MaysPrimera(obtenerNombre.value) == allPokemon[i].nombre && i < allPokemon.length) {
+            llamado = `<div id="especifico">
+             <img src= "${allPokemon[i].imagen}"/></div>
+             <div id="letras">
+             <p><strong>Nombre:</strong> ${allPokemon[i].nombre}</p>
+             <p><strong>Número:</strong> ${allPokemon[i].number}</p>
+             <p><strong>Altura:</strong> ${allPokemon[i].altura}</p>
+             <p><strong>Peso:</strong> ${allPokemon[i].peso}</p>
+             <p><strong>caramelo</strong>: ${allPokemon[i].caramelo}</p>
+             <p><strong>contador de Caramelos:</strong> ${allPokemon[i].contadorDeCaramelos}</p>
+             <p><strong>Huevo:</strong> ${allPokemon[i].huevo}</p>
+             <p><strong>Tipo:</strong> ${allPokemon[i].tipo}</p></div>`;
             break;
         } else {
             llamado = "No se encontraron resultados";
@@ -96,3 +109,28 @@ const mostrarPokemones = (allPokemon) => {
     return mostrar;
 };
 allPokemones.innerHTML = mostrarPokemones(pokemonNew);
+
+//Funcionaliad de ordenar Pokemones de A-z
+const ordenarAz =(allPokemon ) =>{
+    const nombres =[];
+    const ordenados=[];
+    for(let i=0; i < allPokemon.length; i++){
+        nombres[i] = allPokemon[i].nombre;
+    }
+    for(let j=0; j < allPokemon.length; j++){
+    for(let x=0; x < allPokemon.length; x++){
+            /* ordenados.push({
+            'n':allPokemon[j].number,
+            'no': allPokemon[j].nombre,
+            'i': allPokemon[j].imagen,
+            'tip': allPokemon[j].tipo,});*/
+        }
+        
+    }
+    return ordenados;
+
+}
+
+console.log(ordenarAz(pokemonNew));
+
+
