dfb20f6f5abcae803f80eb12f923c9e9985e921d
jorgelisari1 <jorgelis_ari@hotmail.com>
2019-07-09 10:47:08 -0500

    actualizando data con mi compañera

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ const BarraMenu = document.getElementById('barra-menu');
 const BotonIngresar = document.getElementById('BotonIngresar');
 const botonBuscar = document.getElementById('botonBuscar');
 const regresar = document.getElementById('regresar');
-const clickAz = document.getElementById('az');
+
 
 //Variable de la Data
 const pokemonNew = nuevaDataPokemones();
@@ -117,11 +117,14 @@ allPokemones.innerHTML = mostrarPokemones(pokemonNew);
 //Funcionaliad de ordenar Pokemones de A-z
 const ordenarAz =(allPokemon ) =>{
     allPokemon.sort((unPokemon, otroPokemon) => unPokemon.nombre.localeCompare(otroPokemon.nombre));
+    allPokemones.classList.add('hide');
+    ordPokemones.classList.remove('hide');
    return(mostrarPokemones(allPokemon)) ;
 
+   
 }
 
-console.log(ordenarAz(pokemonNew));
+
 
 //Funcionaliad de ordenar Pokemones de Z-a
 const ordenarZa =(allPokemon ) =>{
@@ -129,10 +132,7 @@ const ordenarZa =(allPokemon ) =>{
     return(mostrarPokemones(allPokemon));
 
 }
-clickAz.addEventListener('click',()=>{
-   
-    ordenarAz(pokemonNew);
-});
+
 
 //Funcionalidad de ordenar ascendentemente por frecuencia de aparicion
 
@@ -244,30 +244,7 @@ const  mostrarPorDebilidades= (allPokemon) =>{
 
         return mostrar;
     }
-  
+
+    allPokemones.innerHTML= mostrarPorDebilidades(pokemonNew);
 
  
-    const modal = document.getElementById("myModal");
-    
-    // Get the button that opens the modal
-    const btn = document.getElementById("myBtn");
-    
-    // Get the <span> element that closes the modal
-    const span = document.getElementsByClassName("close")[0];
-    
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
-    
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-    
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
