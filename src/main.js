c40eeb7deedad3eff95b7d60a51758058ea3fd97
jorgelisari1 <jorgelis_ari@hotmail.com>
2019-07-09 08:39:07 -0500

    intentando modal

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ const BarraMenu = document.getElementById('barra-menu');
 const BotonIngresar = document.getElementById('BotonIngresar');
 const botonBuscar = document.getElementById('botonBuscar');
 const regresar = document.getElementById('regresar');
+const clickAz = document.getElementById('az');
 
 //Variable de la Data
 const pokemonNew = nuevaDataPokemones();
@@ -120,12 +121,18 @@ const ordenarAz =(allPokemon ) =>{
 
 }
 
+console.log(ordenarAz(pokemonNew));
+
 //Funcionaliad de ordenar Pokemones de Z-a
 const ordenarZa =(allPokemon ) =>{
     allPokemon.sort((unPokemon, otroPokemon) => otroPokemon.nombre.localeCompare(unPokemon.nombre));
     return(mostrarPokemones(allPokemon));
 
 }
+clickAz.addEventListener('click',()=>{
+   
+    ordenarAz(pokemonNew);
+});
 
 //Funcionalidad de ordenar ascendentemente por frecuencia de aparicion
 
@@ -240,3 +247,27 @@ const  mostrarPorDebilidades= (allPokemon) =>{
   
 
  
+    const modal = document.getElementById("myModal");
+    
+    // Get the button that opens the modal
+    const btn = document.getElementById("myBtn");
+    
+    // Get the <span> element that closes the modal
+    const span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
